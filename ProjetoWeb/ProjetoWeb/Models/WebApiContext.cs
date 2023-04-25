0b287ded9aad6ae54a7e30e1fedcd44fb8e68ab7
SamiraHabib <samirahabib08@gmail.com>
2023-04-25 20:55:01 -0300

    Criação do projeto WebApi e suas dependências

---
+++ ProjetoWeb/ProjetoWeb/Models/WebApiContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoWeb.Models
+{
+    public class WebApiContext : DbContext
+    {
+
+    }
+}
---
+++ ProjetoWeb/ProjetoWeb/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoWeb.Models;
+
+namespace ProjetoWeb
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            
+            services.AddDbContext<WebApiContext>(options =>
+                options.UseSqlServer(connectionString));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjetoWeb v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
